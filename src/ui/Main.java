4ae48244992e026297583e76c6a55295465fb790
lediazc <lediazc@achs.cl>
2026-06-14 21:19:03 -0400

    Se agrega comentario javadoc a main

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -6,7 +6,11 @@ import java.util.Scanner;
 
 public class Main {
 
-
+    /**
+     * Punto de entrada principal del aplicativo.
+     *
+     * @param args Argumento de ejecución.
+     */
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
