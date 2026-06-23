4658705b3c3ab8004996d33ecd8c54f201e9e051
lediazc <lediazc@achs.cl>
2026-06-22 22:17:10 -0400

    Ajuste javadoc

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,7 +6,6 @@ public class Main {
 
     /**
      * Punto de entrada principal del aplicativo.
-     *
      * @param args Argumento de ejecución.
      */
     public static void main(String[] args) {
