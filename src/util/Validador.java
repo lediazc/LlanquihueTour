6f9e176bf8b888aff7fa4a493771c922997997e9
lediazc <lediazc@achs.cl>
2026-07-18 11:33:44 -0400

    Creacion de validación de rut

--- src/util/Validador.java
+++ src/util/Validador.java
@@ -47,4 +47,46 @@ public class Validador {
     public static boolean correoValido(String correo) {
         return correo != null && correo.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
     }
+
+    public static boolean cedulaIdentidadValida(String cedulaIdentidad){
+        return cedulaIdentidad != null && cedulaIdentidad.matches("^[0-9]{7,8}-[0-9Kk]$");
+    }
+
+    public static boolean numeroVerificadorValido(String cedulaIdentidad){
+        int suma = 0;
+        int localizadorGuion = cedulaIdentidad.length() - 2;
+
+        String cuerpoRut = cedulaIdentidad.substring(0, localizadorGuion);
+        String cuerpoRutInv = new StringBuilder(cuerpoRut).reverse().toString();
+        String numeroVerificador = cedulaIdentidad.substring(cedulaIdentidad.length()-1);
+
+
+
+        for(int i = 0; i <cuerpoRutInv.length(); i++){
+            char digito = cuerpoRutInv.charAt(i);
+
+            int digitoInt = Character.getNumericValue(digito);
+
+            int factor = (i % 6) + 2;
+            int multiplicacion = digitoInt * factor;
+
+            suma += multiplicacion;
+
+        }
+
+        int restoCuerpoRUT = suma % 11;
+
+        int digitoVerificadorBruto = 11 - restoCuerpoRUT;
+
+
+        String digitoVerificadorBrutoString = String.valueOf(digitoVerificadorBruto);
+
+        if(digitoVerificadorBrutoString.equalsIgnoreCase("10")){
+            digitoVerificadorBrutoString = "k";
+        }else if(digitoVerificadorBrutoString.equalsIgnoreCase("11")){
+            digitoVerificadorBrutoString = "0";
+        }
+
+        return digitoVerificadorBrutoString.equalsIgnoreCase(numeroVerificador);
+    }
 }
